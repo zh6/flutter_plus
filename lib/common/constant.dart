948899132ac923243ebcb4c190e452f9ddb23759
zh6 <1144941477@qq.com>
2021-07-09 13:43:27 +0800

    删除不要的类文件

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -4,25 +4,13 @@ class Constant {
   static const APP_BAR = 50.0;
   static const DEBUG = true;
   static const PAGE_SIZE = 20;
-  static const BASE_URL = "http://139.9.214.31:8008/";
+  // static const BASE_URL = "http://139.9.214.31:8008/";
+  static const BASE_URL = "http://10.2.15.74:8008/";
   static const TOKEN = "TOKEN";
   static const USER = "USER";
 
   static const accesskey = "";
   static const OSSAccessKeyId = "";
   static const OSSurl = "";
-
-
-
-
-  static const errorRequestCancelled = 'errorRequestCancelled';
-  static const errorConnectionTimeout = 'errorConnectionTimeout';
-  static const errorInternetConnection = 'errorInternetConnection';
-  static const errorReceiveTimeout = 'errorReceiveTimeout';
-  static const errorSendTimeout = 'errorSendTimeout';
-  static const errorBadRequest = 'errorBadRequest';
-  static const errorRequestNotFound = 'errorRequestNotFound';
-  static const errorIntenalServer = 'errorIntenalServer';
-  static const errorSomethingWentWrong = 'errorSomethingWentWrong';
 
 }
